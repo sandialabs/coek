6b9d54bea5a2507b1c96e8c32a12cd66d82462a8
William Hart <whart222@gmail.com>
2018-09-07 18:00:09 -0600

    Adding comments

--- poek/__init__.py
+++ poek/__init__.py
@@ -1,1 +1,18 @@
 # Main __init__.py file
+
+#
+# Need to create the following API
+#
+# create_model()
+# create_variable()
+# add_variable()
+# add_constraint()
+# add_objective()
+# print_model()
+# display_model()
+# SolverFactory()
+#
+# Plus logic for creating simple expressions
+#
+
+
