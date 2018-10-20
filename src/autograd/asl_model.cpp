86e6415cf4e6041f618804b023a25b75cfb9c6d6
William Hart <whart222@gmail.com>
2018-10-19 19:29:44 -0600

    Stubbing in files for ASL and CppAD

---
+++ src/autograd/asl_model.cpp
@@ -0,0 +1,28 @@
+#include "asl_model.h"
+
+void ASL_Model::build()
+{}
+
+double ASL_Model::compute_f(unsigned int i)
+{
+return 0.0;
+}
+
+void ASL_Model::compute_df(double& f, std::vector<double>& df, unsigned int i)
+{}
+
+void ASL_Model::compute_c(std::vector<double>& c)
+{}
+
+void ASL_Model::compute_dc(std::vector<double>& dc, unsigned int i)
+{}
+
+void ASL_Model::compute_adjoints(unsigned int i)
+{}
+
+void ASL_Model::compute_Hv(std::vector<double>& v, std::vector<double>& Hv, unsigned int i)
+{}
+
+void ASL_Model::print(std::ostream& ostr, int df)
+{}
+
