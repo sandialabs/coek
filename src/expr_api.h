9c4e4efbca826740b32a6bcaa10120d2b85da932
William Hart <whart222@gmail.com>
2018-10-19 20:06:14 -0600

    Removing expr_intrinsic.h
    Moved this into expr_api.h

--- src/expr_api.h
+++ src/expr_api.h
@@ -97,3 +97,29 @@ void* get_solver(char* name);
 int solver_solve(void* solver);
 void set_solver_model(void* solver, void* model);
 
+
+
+
+/* Intrinsic Expression Functions */
+
+void* intrinsic_abs(void* expr);
+void* intrinsic_pow(void* expr);
+void* intrinsic_ceil(void* expr);
+void* intrinsic_floor(void* expr);
+void* intrinsic_exp(void* expr);
+void* intrinsic_log(void* expr);
+void* intrinsic_log10(void* expr);
+void* intrinsic_sqrt(void* expr);
+void* intrinsic_sin(void* expr);
+void* intrinsic_cos(void* expr);
+void* intrinsic_tan(void* expr);
+void* intrinsic_sinh(void* expr);
+void* intrinsic_cosh(void* expr);
+void* intrinsic_tanh(void* expr);
+void* intrinsic_asin(void* expr);
+void* intrinsic_acos(void* expr);
+void* intrinsic_atan(void* expr);
+void* intrinsic_asinh(void* expr);
+void* intrinsic_acosh(void* expr);
+void* intrinsic_atanh(void* expr);
+
