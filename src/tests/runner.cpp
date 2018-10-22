dcf7d2d196748246532923a689aeccffcef7b7b1
William <whart222@gmail.com>
2018-10-22 06:07:39 +0000

    Adding Catch2 test infrastructure

---
+++ src/tests/runner.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+unsigned int Factorial(unsigned int number)  {
+  return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorial Test", "[factorial]" ) {
+  REQUIRE( Factorial(2) == 2 );
+}
+
+
