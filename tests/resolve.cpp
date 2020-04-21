182cc047663e947e91af0787f87813d01cfaa817
wehart <whart222@gmail.com>
2020-04-21 16:52:32 -0600

    Allowing resolve with ipopt

--- tests/resolve.cpp
+++ tests/resolve.cpp
@@ -11,16 +11,16 @@
 #include <functional>
 #include "coek_model.hpp"
 
-void baseline_test(int N);
-void objective_test(int N, int nsolves);
-void row_test(int N, int nsolves);
-void col_test(int N, int nsolves);
-void rhs_test(int N, int nsolves);
+void baseline_test(int N, const std::string& solver);
+void objective_test(int N, int nsolves, const std::string& solver);
+void row_test(int N, int nsolves, const std::string& solver);
+void col_test(int N, int nsolves, const std::string& solver);
+void rhs_test(int N, int nsolves, const std::string& solver);
 
 int main(int argc, char** argv)
 {
 if (argc == 1) {
-    std::cout << "resolve <N> <n-solves> <test>" << std::endl;
+    std::cout << "resolve <N> <n-solves> <test> <solver>" << std::endl;
     std::cout << "   Tests: baseline, obj, row, col, rhs" << std::endl;
     return 0;
     }
@@ -28,27 +28,28 @@ if (argc == 1) {
 int N = atoi(argv[1]);          // A is an NxN matrix
 int nsolves = atoi(argv[2]);    // Number of solvers
 std::string test = argv[3];     // Specify the type of test that will be performed
+std::string solver = argv[4];   // Specify the solver used during the tests
 
 if (test == "baseline")
-    baseline_test(N);
+    baseline_test(N, solver);
 
 else if (test == "obj")
-    objective_test(N, nsolves);
+    objective_test(N, nsolves, solver);
 
 else if (test == "row")
-    row_test(N, nsolves);
+    row_test(N, nsolves, solver);
 
 else if (test == "col")
-    col_test(N, nsolves);
+    col_test(N, nsolves, solver);
 
 else if (test == "rhs")
-    rhs_test(N, nsolves);
+    rhs_test(N, nsolves, solver);
 
 return 0;
 }
 
 
-void baseline_test(int N)
+void baseline_test(int N, const std::string& solver)
 {
 std::mt19937 rng(10000) ;
 std::uniform_real_distribution<double> distribution(0,1);
@@ -71,7 +72,7 @@ for (int n=0; n<N; n++)
 
 std::vector<coek::Variable> x(N);
 for (int n=0; n<N; n++)
-    x[n] = model.getVariable(0, COEK_INFINITY, 0);
+    x[n] = model.getVariable(0, COEK_INFINITY, 1);
 
 // obj
 coek::Expression obj;
@@ -85,18 +86,28 @@ for (int n=0; n<N; n++) {
     for (int m=0; m<N; m++)
         expr += A[n][m] * x[m];
     model.add( expr <= b[n] );
+    break;
     }
 
 
-coek::Solver opt;
-opt.initialize("gurobi");
-//opt.set_option("TimeLimit", 0);
-opt.solve(model);
-std::cout << "Solve: " << 0 << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+if (solver == "gurobi") {
+    coek::Solver opt(solver);
+    //opt.set_option("TimeLimit", 0);
+    opt.solve(model);
+    std::cout << "Solve: " << 0 << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+    }
+else {
+    coek::NLPModel nlp(model, "cppad");
+    std::cout << "NLP\n" << nlp << std::endl;
+    coek::NLPSolver opt("ipopt");
+    opt.set_option("print_level", 0);
+    opt.solve(nlp);
+    std::cout << "Solve: " << 0 << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+    }
 }
 
 
-void objective_test(int N, int nsolves)
+void objective_test(int N, int nsolves, const std::string& solver)
 {
 std::mt19937 rng(10000) ;
 std::uniform_real_distribution<double> distribution(0,1);
@@ -136,22 +147,35 @@ for (int n=0; n<N; n++) {
     }
 
 
-coek::Solver opt;
-opt.initialize("gurobi");
-//opt.set_option("TimeLimit", 0);
-opt.load(model);
-for (int i=0; i<nsolves; i++) {
-    opt.resolve();
-    std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+if (solver == "gurobi") {
+    coek::Solver opt(solver);
+    opt.load(model);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    for (int n=0; n<N; n++)
-        c[n].set_value( uniform() );
-    std::cout << std::endl;
+        for (int n=0; n<N; n++)
+            c[n].set_value( uniform() );
+        std::cout << std::endl;
+        }
+    }
+else {
+    coek::NLPModel nlp(model, "cppad");
+    coek::NLPSolver opt("ipopt");
+    opt.load(nlp);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+
+        for (int n=0; n<N; n++)
+            c[n].set_value( uniform() );
+        std::cout << std::endl;
+        }
     }
 }
 
 
-void row_test(int N, int nsolves)
+void row_test(int N, int nsolves, const std::string& solver)
 {
 // Make this row have random values
 int row = std::min(10, N-1);
@@ -204,23 +228,35 @@ for (int n=0; n<N; n++) {
     }
 
 
-coek::Solver opt;
-opt.initialize("gurobi");
-//opt.set_option("TimeLimit", 0);
-opt.load(model);
-for (int i=0; i<nsolves; i++) {
-    opt.resolve();
-    std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+if (solver == "gurobi") {
+    coek::Solver opt(solver);
+    opt.load(model);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    for (int m=0; m<N; m++)
-        A_[m].set_value( uniform() );
+        for (int m=0; m<N; m++)
+            A_[m].set_value( uniform() );
+        std::cout << std::endl;
+        }
+    }
+else {
+    coek::NLPModel nlp(model, "cppad");
+    coek::NLPSolver opt("ipopt");
+    opt.load(nlp);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    std::cout << std::endl;
+        for (int m=0; m<N; m++)
+            A_[m].set_value( uniform() );
+        std::cout << std::endl;
+        }
     }
 }
 
 
-void col_test(int N, int nsolves)
+void col_test(int N, int nsolves, const std::string& solver)
 {
 // Make this col have random values
 int col = std::min(10, N-1);
@@ -272,23 +308,35 @@ for (int n=0; n<N; n++) {
     }
 
 
-coek::Solver opt;
-opt.initialize("gurobi");
-//opt.set_option("TimeLimit", 0);
-opt.load(model);
-for (int i=0; i<nsolves; i++) {
-    opt.resolve();
-    std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+if (solver == "gurobi") {
+    coek::Solver opt(solver);
+    opt.load(model);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    for (int n=0; n<N; n++)
-        A_[n].set_value( uniform() );
+        for (int n=0; n<N; n++)
+            A_[n].set_value( uniform() );
+        std::cout << std::endl;
+        }
+    }
+else {
+    coek::NLPModel nlp(model, "cppad");
+    coek::NLPSolver opt("ipopt");
+    opt.load(nlp);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    std::cout << std::endl;
+        for (int n=0; n<N; n++)
+            A_[n].set_value( uniform() );
+        std::cout << std::endl;
+        }
     }
 }
 
 
-void rhs_test(int N, int nsolves)
+void rhs_test(int N, int nsolves, const std::string& solver)
 {
 std::mt19937 rng(10000) ;
 std::uniform_real_distribution<double> distribution(0,1);
@@ -328,17 +376,29 @@ for (int n=0; n<N; n++) {
     }
 
 
-coek::Solver opt;
-opt.initialize("gurobi");
-//opt.set_option("TimeLimit", 0);
-opt.load(model);
-for (int i=0; i<nsolves; i++) {
-    opt.resolve();
-    std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
+if (solver == "gurobi") {
+    coek::Solver opt(solver);
+    opt.load(model);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    for (int n=0; n<N; n++)
-        b[n].set_value( uniform() );
+        for (int n=0; n<N; n++)
+            b[n].set_value( uniform() );
+        std::cout << std::endl;
+        }
+    }
+else {
+    coek::NLPModel nlp(model, "cppad");
+    coek::NLPSolver opt("ipopt");
+    opt.load(nlp);
+    for (int i=0; i<nsolves; i++) {
+        opt.resolve();
+        std::cout << "Solve: " << i << "  Objective: " << model.repn->objectives[0].get_value() << std::endl;
 
-    std::cout << std::endl;
+        for (int n=0; n<N; n++)
+            b[n].set_value( uniform() );
+        std::cout << std::endl;
+        }
     }
 }
