d9e95002525f0117e780740aa92a10cdc39716a2
wehart <whart222@gmail.com>
2020-04-22 14:52:24 -0600

    Adding an inverse quadratic
    The quadratic is centered about values
    specified by parameters, so this can be used to test
    resolves.

---
+++ examples/invquad.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include "coek_model.hpp"
+
+void invquad_example(coek::Model& m, std::vector<coek::Parameter>& p)
+{
+std::vector<coek::Variable> x(p.size());
+for (auto it=x.begin(); it != x.end(); ++it) {
+    *it = coek::Variable(-10, 10, 0);
+    m.addVariable(*it);
+    }
+
+coek::Expression e;
+for (size_t i=0; i<x.size(); i++)
+    e += (x[i]-p[i])*(x[i]-p[i]);
+
+m.add( -e );
+}
