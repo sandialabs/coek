0f909e061f388b57a5264a5f2c1858ca065b428c
wehart <whart222@gmail.com>
2020-06-29 15:35:36 -0600

    Several changes
    1. Updating due to API changes in COEK
    
    2. Recognize cppyy and pybind11 interfaces
        separately

---
+++ poek/func.py
@@ -0,0 +1,31 @@
+
+def prod(v):
+    expr = 1
+    for v_ in v.values():
+        expr *= v_
+    return expr
+
+def quicksum(args, start=0):
+    """
+    A utility function to compute a sum of Poek expressions.
+
+    The behavior of :func:`quicksum` is similar to the builtin :func:`sum`
+    function, but this function generates a more compact Poek
+    expression.
+
+    Args:
+        args: A generator for terms in the sum.
+
+        start: A value that is initializes the sum.  If
+            this value is not a numeric constant, then the +=
+            operator is used to add terms to this object.
+            Defaults to zero.
+
+    Returns:
+        The value of the sum, which may be a Poek expression object.
+    """
+    e = start
+    for arg in args:
+        e += arg
+    return e
+
