75f1d9baa59363f2c01bacf12aabd9c381300c14
wehart <whart222@gmail.com>
2020-06-29 20:18:36 -0600

    Fixing interface

--- poek/poek_pybind11.py
+++ poek/poek_pybind11.py
@@ -1,32 +1,32 @@
 #
 # Pull-in Pybind11 wrappers
 #
-from pycoek import _variable
-from pycoek import *
+from pycoek_pybind11 import variable_
+from pycoek_pybind11 import *
 from .func import quicksum, prod
 
 class variable(object):
 
     def __new__(cls, *args, **kwds):
         if len(args) == 0 or args[0] == 1 or type(args[0]) == str:
-            return _variable(**kwds)
+            return variable_(**kwds)
         elif args[0].__class__ == ConcreteSet:
             return IndexedVariable(*args)
-        return _variable(args[0], **kwds)
+        return variable_(args[0], **kwds)
 
 
 def model_variable(self, *args, **kwds):
         if len(args) == 0 or args[0] == 1 or type(args[0]) == str:
             if 'index' in kwds:
                 _index = kwds.pop('index')
-                v = _variable(len(_index), **kwds)
+                v = variable_(len(_index), **kwds)
                 self.use(v)
                 ans = {}
                 for i, key in enumerate(_index):
                     ans[key] = v[i]
                 return ans
             else:
-                v =_variable(**kwds)
+                v = variable_(**kwds)
                 self.use(v)
                 return v
         elif args[0].__class__ == ConcreteSet:
@@ -34,7 +34,7 @@ def model_variable(self, *args, **kwds):
             self.use(v)
             return v
         else:
-            v = _variable(args[0], **kwds)
+            v = variable_(args[0], **kwds)
             self.use(v)
             return v
 
