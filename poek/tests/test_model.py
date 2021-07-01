e8916bd08276671859d41948342aa189bffbf741
whart222 <whart222@gmail.com>
2021-07-01 07:49:48 -0600

    Testing the NL writer

--- poek/tests/test_model.py
+++ poek/tests/test_model.py
@@ -63,6 +63,23 @@ class TestModel(unittest.TestCase):
         m.add_variable(v)
         m.add_objective(v)
 
+    def test_write_nl(self):
+        m = model()
+        v1 = m.add_variable(name="v1", value=1)
+        v2 = m.add_variable(name="v2", value=2)
+        v3 = m.add_variable(name="v3", value=3)
+        #v4 = variable(name="v3", value=3)
+
+        e = v1+v2
+        m.add_objective( (1-e) + 3*e + e*e + v3 )
+        m.add_constraint( v1*v1+1 <= 3*v2 )
+        m.add_constraint( v1**2 >= 3*v2 )
+        m.add_constraint( 1 < v2 )
+        m.add_constraint( v1 > 1 )
+        m.add_constraint( v1 == 3*v3-3 )
+
+        m.write("test1.nl")
+
 
 if __name__ == "__main__":      #pragma:nocover
     unittest.main()
