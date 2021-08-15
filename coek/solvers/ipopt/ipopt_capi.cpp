3e8d8ddb3ca3323b9dc2e678c982980a072c8428
William Hart <728056+whart222@users.noreply.github.com>
2021-08-15 16:05:15 -0400

    Update ipopt_capi.cpp

--- coek/solvers/ipopt/ipopt_capi.cpp
+++ coek/solvers/ipopt/ipopt_capi.cpp
@@ -12,6 +12,74 @@
 
 extern "C" {
 
+ /* Function Declarations */
+Bool eval_f(
+   Index       n,
+   Number*     x,
+   Bool        new_x,
+   Number*     obj_value,
+   UserDataPtr user_data
+);
+
+Bool eval_grad_f(
+   Index       n,
+   Number*     x,
+   Bool        new_x,
+   Number*     grad_f,
+   UserDataPtr user_data
+);
+
+Bool eval_g(
+   Index       n,
+   Number*     x,
+   Bool        new_x,
+   Index       m,
+   Number*     g,
+   UserDataPtr user_data
+);
+
+Bool eval_jac_g(
+   Index       n,
+   Number*     x,
+   Bool        new_x,
+   Index       m,
+   Index       nele_jac,
+   Index*      iRow,
+   Index*      jCol,
+   Number*     values,
+   UserDataPtr user_data
+);
+
+Bool eval_h(
+   Index       n,
+   Number*     x,
+   Bool        new_x,
+   Number      obj_factor,
+   Index       m,
+   Number*     lambda,
+   Bool        new_lambda,
+   Index       nele_hess,
+   Index*      iRow,
+   Index*      jCol,
+   Number*     values,
+   UserDataPtr user_data
+);
+
+Bool intermediate_cb(
+   Index       alg_mod,
+   Index       iter_count,
+   Number      obj_value,
+   Number      inf_pr,
+   Number      inf_du,
+   Number      mu,
+   Number      d_norm,
+   Number      regularization_size,
+   Number      alpha_du,
+   Number      alpha_pr,
+   Index       ls_trials,
+   UserDataPtr user_data
+);
+ 
 };
 
 namespace coek {
