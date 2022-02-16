7c88104d99333cd7f7ca9f071f38f65f100af3ed
William Hart <whart222@gmail.com>
2022-02-16 07:31:50 -0500

    Adding example using 'bounds'.

--- examples/simplelp1.cpp
+++ examples/simplelp1.cpp
@@ -2,8 +2,8 @@
 
 void simplelp1_example(coek::Model& m)
 {
-auto x = m.add_variable("x").lower(0).upper(m.inf);
-auto y = m.add_variable("y").lower(0).upper(m.inf);
+auto x = m.add_variable("x").bounds(0,m.inf);
+auto y = m.add_variable("y").bounds(0,m.inf);
 
 m.add_objective( 50*x + 40*y).sense( coek::Model::maximize );
 m.add_constraint( 2*x + 3*y <= 1500 );
