2d2efb001c3510ff980ed42e62e547bb2c6ab619
Michael Bynum <mlbynum@sandia.gov>
2022-08-03 12:46:59 -0600

    working on Objective

--- pyomo_coek/__init__.py
+++ pyomo_coek/__init__.py
@@ -1,3 +1,4 @@
-from . import variable, constraint
+from . import variable, constraint, objective
 from .variable import Var, ScalarVar, IndexedVar
 from .constraint import Constraint, ScalarConstraint, IndexedConstraint
+from .objective import Objective, ScalarObjective, IndexedObjective
