8976097e2b98dbe2fda52493b0221b83e24b035a
William Hart <whart222@gmail.com>
2022-08-04 11:47:33 -0400

    Adding logic to run solve0 tests

--- scripts/test_coek.py
+++ scripts/test_coek.py
@@ -36,7 +36,7 @@ all_coek_models = [
     "lqcp_scalar",
     "lqcp_array",
     "lqcp_map",
-    "lqcp_compact"
+    #"lqcp_compact"         Problem constructing this model
     ]
 
 all_suffixes = ['lp', 'nl']
@@ -44,6 +44,7 @@ all_suffixes = ['lp', 'nl']
 
 
 def test_writer(*, test_type, models, suffixes, executable):
+    errors=[]
     if not os.path.exists("results"):
         os.mkdir("results")
 
@@ -81,15 +82,85 @@ def test_writer(*, test_type, models, suffixes, executable):
                 cmd = [model] + [test_args]
             for suffix in suffixes:
                 outfile = "results/"+ "_".join(cmd+[suffix]) + "." + suffix
-                logfile = "results/"+ "_".join(cmd+[suffix]) + ".out"
+                timefile = "results/"+ "_".join(cmd+[suffix]) + ".out"
 
-                if os.path.exists(logfile):
-                    print(". Skipping test {}-{}: logfile exists".format(model, suffix))
+                if os.path.exists(timefile):
+                    print(". Skipping test {}: results exist".format(timefile))
                     continue
 
-                run = ["/usr/bin/time", "-p", "-o", logfile, executable, outfile]+cmd
+                run = ["/usr/bin/time", "-p", "-o", timefile, executable, outfile]+cmd
                 results = subprocess.run(run, capture_output=True)
-                print(". Test complete {}: rc={} output='{}'".format(logfile, results.returncode, results.stdout))
+                print(". Test complete {}: rc={} output='{}'".format(timefile, results.returncode, results.stdout))
+                if results.returncode:
+                    if os.path.exists(timefile):
+                        os.remove(timefile)
+                    errors.append(timefile)
+
+    if len(errors) > 0:
+        print("\n\nERRORS")
+        for f in errors:
+            print("  ",f)
+
+
+def test_solve0(*, test_type, models, solvers, executable):
+    errors=[]
+    if not os.path.exists("results"):
+        os.mkdir("results")
+
+    print("")
+    print("TESTING ARGUMENTS")
+    print("  test_type: "+ test_type)
+    print("  models:    "+ " ".join(models))
+    print("  suffixes:  "+ " ".join(suffixes))
+    print("")
+
+    for model in models:
+        config_name = None
+        for name in config:
+            if model.startswith(name):
+                config_name = name
+        if config_name is None:
+            print("Ignoring unknown model {}".format(model))
+            continue
+
+        print("\nTesting model {}".format(model))
+        if test_type == "all":
+            tests = []
+            for v in config[config_name].values():
+                tests += v
+        elif test_type in config[config_name]:
+            tests = config[config_name][test_type]
+        else:
+            print("Unknown test type {}".format(test_type))
+            continue
+
+        for test_args in tests:
+            if type(test_args) is list:
+                cmd = [model] + test_args
+            else:
+                cmd = [model] + [test_args]
+            for solver in solvers:
+                logfile = "results/"+ "_".join(cmd+[solver]) + ".log"
+                timefile = "results/"+ "_".join(cmd+[solver]) + ".out"
+
+                if os.path.exists(timefile):
+                    print(". Skipping test {}: timefile exists".format(timefile))
+                    continue
+
+                run = ["/usr/bin/time", "-p", "-o", timefile, executable, solver]+cmd
+                results = subprocess.run(run, capture_output=True)
+                with open(logfile,'wb') as OUTPUT:
+                    OUTPUT.write(results.stdout)
+                print(". Test complete {}: rc={}".format(timefile, results.returncode))
+                if results.returncode:
+                    if os.path.exists(timefile):
+                        os.remove(timefile)
+                    errors.append(timefile)
+
+    if len(errors) > 0:
+        print("\n\nERRORS")
+        for f in errors:
+            print("  ",f)
 
 
 if __name__ == "__main__":
@@ -98,5 +169,7 @@ if __name__ == "__main__":
     #models = ["knapsack_scalar"]
     models = all_coek_models
     suffixes = all_suffixes
-    test_writer(test_type=test_type, models=models, suffixes=suffixes, executable="coek/coek_writer")
+    solvers = ['ipopt']
+    #test_writer(test_type=test_type, models=models, suffixes=suffixes, executable="coek/coek_writer")
+    test_solve0(test_type=test_type, models=models, solvers=solvers, executable="coek/coek_solve0")
 
