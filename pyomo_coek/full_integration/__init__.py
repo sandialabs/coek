ee9a8fc2bf5307fa77a4b9995e21c5bc72ec21b9
Michael Bynum <mlbynum@sandia.gov>
2022-08-30 20:42:07 -0600

    update __init__

--- pyomo_coek/full_integration/__init__.py
+++ pyomo_coek/full_integration/__init__.py
@@ -1,9 +1,9 @@
 from . import expression, constraint, objective
-from .expression import Var, ScalarVar, IndexedVar, quicksum, LinearExpression
+from .expression import Var, ScalarVar, IndexedVar, quicksum, LinearExpression, Param, IndexedParam, ScalarParam
 from .standard_repn import _generate_standard_repn
 from .constraint import Constraint, ScalarConstraint, IndexedConstraint
 from .objective import Objective, ScalarObjective, IndexedObjective
 from .solvers.gurobi import Gurobi
 from pyomo.core.base.PyomoModel import ConcreteModel
-from pyomo.core.base.set import RangeSet, Binary, Set
+from pyomo.core.base.set import RangeSet, Binary, Set, Reals
 from pyomo.opt.base.solvers import SolverFactory
