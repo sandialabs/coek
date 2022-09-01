f59da9641eae9b484e7cec426177d8b7b40ef88e
Michael Bynum <mlbynum@sandia.gov>
2022-09-01 06:40:54 -0600

    solver interface updates

--- pyomo_coek/components_only/__init__.py
+++ pyomo_coek/components_only/__init__.py
@@ -2,7 +2,7 @@ from . import variable, constraint, objective
 from .variable import Var, ScalarVar, IndexedVar, Param, ScalarParam, IndexedParam
 from .constraint import Constraint, ScalarConstraint, IndexedConstraint
 from .objective import Objective, ScalarObjective, IndexedObjective
-from .solvers import Gurobi
+from .solvers import Gurobi, Ipopt
 from pyomo.core.base.PyomoModel import ConcreteModel
 from pyomo.core.base.set import RangeSet, Binary, Set, Reals
 from pyomo.core.base.objective import minimize, maximize
--- pyomo_coek/components_only/solvers.py
+++ pyomo_coek/components_only/solvers.py
@@ -1,5 +1,6 @@
 from pyomo.contrib.appsi.base import (
 TerminationCondition,
+SolverConfig,
 MIPSolverConfig,
 SolutionLoader,
 Results,
@@ -16,11 +17,16 @@ from pyomo_coek.components_only.constraint import Constraint
 from pyomo_coek.components_only.variable import Var
 
 
-class Gurobi(Solver):
-    def __init__(self):
-        self._opt = pk.solver('gurobi')
-        self._config = MIPSolverConfig()
+_nlp_solvers = {'ipopt',}
+
+
+class HybridSolver(Solver):
+    def __init__(self, solver_name):
+        self._solver_name = solver_name
+        self._opt = None
         self._solver_options = dict()
+        self._poek_model = None
+        self._poek_nlp_model = None
 
     def available(self):
         if self._opt.available:
@@ -32,24 +38,16 @@ class Gurobi(Solver):
         raise NotImplementedError('ToDo')
 
     @property
-    def config(self):
-        return self._config
+    def options(self):
+        return self._solver_options
 
-    @config.setter
-    def config(self, val):
-        self._config = val
+    @options.setter
+    def options(self, val: Dict):
+        self._solver_options = val
 
     @property
     def symbol_map(self):
         raise NotImplementedError('ToDo')
-
-    @property
-    def gurobi_options(self):
-        return self._solver_options
-
-    @gurobi_options.setter
-    def gurobi_options(self, val: Dict):
-        self._solver_options = val
 
     def _construct_poek_model(
             self, model: _BlockData, timer: HierarchicalTimer
@@ -61,9 +59,14 @@ class Gurobi(Solver):
         for c in model.component_data_objects(Constraint, active=True, descend_into=True):
             pm.add_constraint(c._pe)
         for obj in model.component_data_objects(Objective, active=True, descend_into=True):
-            pm.add_objective(obj._pe, obj.sense == minimize)
+            if obj.sense == minimize:
+                obj_expr = obj._pe
+            else:
+                obj_expr = -obj._pe
+            pm.add_objective(obj_expr, True)
         timer.stop('construct poek model')
 
+        self._poek_model = pm
         return pm
 
     def load(self, model: _BlockData, timer: HierarchicalTimer = None):
@@ -71,16 +74,19 @@ class Gurobi(Solver):
             timer = HierarchicalTimer()
         pm = self._construct_poek_model(model, timer)
         timer.start('coek load')
-        self._opt.load(pm)
+
+        if self._solver_name in _nlp_solvers:
+            self._opt = pk.nlp_solver(self._solver_name)
+            nlp = pk.nlp_model(pm, "cppad")
+            self._poek_nlp_model = nlp
+            self._opt.load(nlp)
+        else:
+            self._opt = pk.solver(self._solver_name)
+            self._opt.load(pm)
         timer.stop('coek load')
 
     def _set_options(self):
-        if self.config.stream_solver:
-            self._opt.set_option("OutputFlag", 1)
-        else:
-            self._opt.set_option("OutputFlag", 0)
-
-        for key, option in self.gurobi_options.items():
+        for key, option in self.options.items():
             self._opt.set_option(key, option)
 
     def resolve(self, timer: HierarchicalTimer = None):
@@ -102,15 +108,82 @@ class Gurobi(Solver):
 
         pm = self._construct_poek_model(model, timer)
 
-        self._set_options()
-
         timer.start('coek solve')
-        _res = self._opt.solve(pm)
+        if self._solver_name in _nlp_solvers:
+            self._opt = pk.nlp_solver(self._solver_name)
+            nlp = pk.nlp_model(pm, "cppad")
+            self._poek_nlp_model = nlp
+            self._set_options()
+            _res = self._opt.solve(nlp)
+        else:
+            self._opt = pk.solver(self._solver_name)
+            self._set_options()
+            _res = self._opt.solve(pm)
         timer.stop('coek solve')
 
         res = Results()
         return res
 
 
+class Gurobi(HybridSolver):
+    def __init__(self):
+        super().__init__('gurobi')
+        self._config = MIPSolverConfig()
+
+    @property
+    def config(self):
+        return self._config
+
+    @config.setter
+    def config(self, val):
+        self._config = val
+
+    @property
+    def gurobi_options(self):
+        return self._solver_options
+
+    @gurobi_options.setter
+    def gurobi_options(self, val: Dict):
+        self._solver_options = val
+
+    def _set_options(self):
+        if self.config.stream_solver:
+            self._opt.set_option("OutputFlag", 1)
+        else:
+            self._opt.set_option("OutputFlag", 0)
+        super()._set_options()
+
+
+class Ipopt(HybridSolver):
+    def __init__(self):
+        super().__init__('ipopt')
+        self._config = SolverConfig()
+
+    @property
+    def config(self):
+        return self._config
+
+    @config.setter
+    def config(self, val):
+        self._config = val
+
+    @property
+    def ipopt_options(self):
+        return self._solver_options
+
+    @ipopt_options.setter
+    def ipopt_options(self, val: Dict):
+        self._solver_options = val
+
+    def _set_options(self):
+        if self.config.stream_solver:
+            self._opt.set_option("print_level", 5)
+        else:
+            self._opt.set_option("print_level", 0)
+        super()._set_options()
+
+
 SolverFactory.register(name='coek_gurobi',
                        doc='Coek interface to Gurobi')(Gurobi)
+SolverFactory.register(name='coek_ipopt',
+                       doc='Coek interface to Gurobi')(Ipopt)
