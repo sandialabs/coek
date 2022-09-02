8f7484a56161924656005fb1217ab8eb6d06d85a
Michael Bynum <michaelbynum@users.noreply.github.com>
2022-09-02 10:24:56 -0600

    updating imports

--- pyomo_coek/components_only/__init__.py
+++ pyomo_coek/components_only/__init__.py
@@ -4,7 +4,7 @@ from .constraint import Constraint, ScalarConstraint, IndexedConstraint
 from .objective import Objective, ScalarObjective, IndexedObjective
 from .solvers import Gurobi, Ipopt
 from pyomo.core.base.PyomoModel import ConcreteModel
-from pyomo.core.base.set import RangeSet, Binary, Set, Reals
+from pyomo.core.base.set import RangeSet, Binary, Set, Reals, NonNegativeReals
 from pyomo.core.base.objective import minimize, maximize
 from pyomo.opt.base.solvers import SolverFactory
 from .quicksum import quicksum, construct_linear_expression
