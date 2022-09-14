48032add5e34e6ac918744c399de1775b5534e66
William Hart <whart222@gmail.com>
2022-09-13 23:07:53 -0400

    Bug fix?

--- doc/coek_doc_setup.sh
+++ doc/coek_doc_setup.sh
@@ -3,14 +3,18 @@
 mamba env create -p rtd -f doc/environment.yml
 #conda activate `pwd`/rtd
 
+mkdir -f _readthedocs
+
 if [[ $1 == "coek" ]]; then
     echo "Running doxygen";
     mkdir _build
     cd _build
     ../rtd/bin/cmake -Dwith_docs=ON ../lib/coek
-    make
+    make docs
     pwd
     ls
+    ls docs
+    mv docs/sphinx/html ../_readthedocs
 fi
 if [[ $1 == "poek" ]]; then
     echo "Running doxygen";
