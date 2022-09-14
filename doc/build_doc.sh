bd2117e2fac40141e0d6c7d037ca44dff368476e
William Hart <whart222@gmail.com>
2022-09-13 23:39:17 -0400

    Trying again

--- doc/build_doc.sh
+++ doc/build_doc.sh
@@ -3,7 +3,7 @@
 # build_doc.sh <PROJECT>
 #
 
-##mamba env create -p rtd -f doc/environment.yml
+mamba env create -p rtd -f doc/environment.yml
 
 [ -d _readthedocs/. ] || mkdir _readthedocs
 
@@ -11,14 +11,14 @@ if [[ $1 == "coek" ]]; then
     echo "Running doxygen";
     mkdir _build
     cd _build
-    ##../rtd/bin/cmake -Dwith_docs=ON ../lib/coek
-    ##make sphinx
+    ../rtd/bin/cmake -Dwith_docs=ON ../lib/coek
+    make sphinx
     pwd
     #ls
     #ls doc
     #ls doc/sphinx
-    echo "UP"
-    ls ..
+    #echo "UP"
+    #ls ..
     mv doc/sphinx ../_readthedocs/html
 fi
 if [[ $1 == "poek" ]]; then
