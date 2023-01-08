8584c9d61400298266794a47b39e803d9b0f4a36
whart222 <whart222@gmail.com>
2023-01-08 13:02:14 -0700

    Misc reformatting of comments

--- lib/coek/coek/ast/value_terms.cpp
+++ lib/coek/coek/ast/value_terms.cpp
@@ -14,16 +14,17 @@ double evaluate_expr(const BaseExpressionTerm* expr,
 double BaseExpressionTerm::eval() const
 {
 //
-// The default behavior here is to use the new evaluate_expr() function.  This should have minimal overhead
-// for expressions without SubExpression terms.  For expressions with SubExpression terms that are repeated, 
-// this has the potential to significantly minimize total cost of the evaluation.
-// 
-// WEH - Testing indicates that this may have a small, but noticable performance degredation, even in cases
-// where subexpressions aren't used.  I think this relates to frequent calls to evaluate_expr(), where we
-// setup temporary data structures.  These aren't used in the _eval() logic.
+// The default behavior here is to use the new evaluate_expr() function.  This should have minimal
+// overhead for expressions without SubExpression terms.  For expressions with SubExpression terms
+// that are repeated, this has the potential to significantly minimize total cost of the evaluation.
 //
-// WEH - For now, I'm leaving the default behavior as not recognizing subexpressions.  We can judiciously use the
-// evaluate_expr() function in cases where we evaluate many expressions.
+// WEH - Testing indicates that this may have a small, but noticable performance degredation, even
+// in cases where subexpressions aren't used.  I think this relates to frequent calls to
+// evaluate_expr(), where we setup temporary data structures.  These aren't used in the _eval()
+// logic.
+//
+// WEH - For now, I'm leaving the default behavior as not recognizing subexpressions.  We can
+// judiciously use the evaluate_expr() function in cases where we evaluate many expressions.
 //
 #if 0
     std::map<std::shared_ptr<SubExpressionTerm>, double> cache;
