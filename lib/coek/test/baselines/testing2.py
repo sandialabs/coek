e1efaf622e927f3a58a1d590e0d5d9ed26995dce
whart222 <whart222@gmail.com>
2023-11-10 10:27:57 -0700

    Updating NL baselines
    Fixed the AMPL header declarations
    to resolve discrepancies with Pyomo examples

---
+++ lib/coek/test/baselines/testing2.py
@@ -0,0 +1,13 @@
+import pyomo.environ as pyo
+
+m = pyo.ConcreteModel()
+
+m.a = pyo.Var(bounds=(0,1), within=pyo.Integers, initialize=0)
+m.b = pyo.Var(bounds=(0,1), within=pyo.Binary, initialize=0)
+m.q = pyo.Param(initialize=2)
+
+m.o = pyo.Objective(expr=3*m.a + m.q + m.a*m.a*m.a*(-m.a + m.b + 3*m.a + 3*m.b + pyo.sin(-pyo.cos(m.a))))
+
+m.b.fix(1)
+
+m.write("testing2.nl")
