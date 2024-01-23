76db0a0ce320c9595723092ba90c3d45c5c74c9a
whart222 <whart222@gmail.com>
2024-01-23 15:33:34 -0700

    Adding 'pyomo' tests

---
+++ lib/pyomo_coek/pyomo_coek/pyomo/__init__.py
@@ -0,0 +1,7 @@
+# The pyomo_coek.pyomo package is a pyomo wrapper that 
+# adds other utility functions used to support testing.
+
+from pyomo.environ import *
+
+def to_list(e):
+    return []
--- lib/pyomo_coek/pyomo_coek/test/conftest.py
+++ lib/pyomo_coek/pyomo_coek/test/conftest.py
@@ -6,7 +6,9 @@ import pytest
 @pytest.fixture(
     scope="session",
     #params=["pyomo_coek.components_only", "pyomo_coek.full_integration"],
-    params=["pyomo_coek.components_only"],
+    #params=["pyomo_coek.components_only"],
+    #params=["pyomo_coek.pyomo", "pyomo_coek.components_only"],
+    params=["pyomo_coek.pyomo"],
 )
 def pyomo_module(request):
     return pytest.importorskip(request.param)
