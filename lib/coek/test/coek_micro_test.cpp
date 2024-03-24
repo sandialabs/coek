aabce2a45e6b0d717095dfc6ad74f634c5fe46d5
whart222 <whart222@gmail.com>
2024-03-24 05:59:14 -0600

    Another micro test

--- lib/coek/test/coek_micro_test.cpp
+++ lib/coek/test/coek_micro_test.cpp
@@ -20,9 +20,10 @@ int main(int argc, char** argv)
 
     if (n == 0) {
         // Benchmark loops
-        size_t m = atoll(argv[2]);  // Test No
+        size_t m = atoll(argv[2]);  // Test Num
         std::cout << "Timing Loops: " << m << std::endl;
         double val=0.0;
+
         coek::tic();
         for (size_t i=0; i<m; i++)
             val += 1.1;
@@ -42,6 +43,50 @@ int main(int argc, char** argv)
             val += 1.1;
         coek::toc("For loop with coek::range<size_t>(0,m,1)");
         std::cout << val << std::endl;
+        }
+
+    else if (n == 1) {
+        // Benchmark var initialization
+        size_t m = atoll(argv[2]);  // Test Num
+        std::cout << "Timing var initialization: " << m << std::endl;
+
+        {
+        coek::tic();
+        std::vector<coek::Variable> x(m);
+        for (size_t i=0; i<m; i++)
+            x[i].value(1.0).lower(0.0).upper(1.0);
+        double val = x[0].value();
+        coek::toc("Loop over indices of std::vector<coek::Variable>");
+        std::cout << x.size() << " " << val << std::endl;
+        }
+
+        {
+        coek::tic();
+        std::vector<coek::Variable> x(m);
+        for (auto& var : x)
+            var.value(1.0).lower(0.0).upper(1.0);
+        double val = x[0].value();
+        coek::toc("Range loop over std::vector<coek::Variable>");
+        std::cout << x.size() << " " << val << std::endl;
+        }
+
+        {
+        coek::tic();
+        auto x = coek::variable(m);
+        for (size_t i=0; i<m; i++)
+            x(i).value(1.0).lower(0.0).upper(1.0);
+        double val = x(0).value();
+        coek::toc("Loop over indices of VariableArray");
+        std::cout << x.size() << " " << val << std::endl;
+        }
+
+        {
+        coek::tic();
+        auto x = coek::variable(m).value(1.0).lower(0.0).upper(1.0);
+        double val = x(0).value();
+        coek::toc("Loop over indices of VariableArray");
+        std::cout << x.size() << " " << val << std::endl;
+        }
         }
 
     return 0;
