485c01f517ed2c7fa70825598513fa4dac4f67ff
whart222 <whart222@gmail.com>
2024-04-21 13:40:09 -0600

    Avoid re-generating Spack install

--- build_dev.sh
+++ build_dev.sh
@@ -6,18 +6,29 @@
 # This uses Spack to install third-party dependencies in the `spack` directory.
 #
 export SPACK_HOME=`pwd`/spack
-echo ${SPACK_HOME}
-\rm -Rf spack
-git clone git@github.com:or-fusion/spack.git
-. ${SPACK_HOME}/share/spack/setup-env.sh
-spack external find python
-spack external find gurobi
-spack env create coekenv
-spack env activate coekenv
-spack add asl cppad fmt rapidjson catch2
-spack install
-spack env deactivate
+echo "SPACK_HOME=${SPACK_HOME}"
 
+if test -d ${SPACK_HOME}; then
+    echo ""
+    echo "WARNING: Spack directory exists."
+    echo ""
+else
+    echo ""
+    echo "Installing Coek dependencies using Spack"
+    echo ""
+    git clone git@github.com:or-fusion/spack.git
+    . ${SPACK_HOME}/share/spack/setup-env.sh
+    spack external find python
+    spack external find gurobi
+    spack env create coekenv
+    spack env activate coekenv
+    spack add asl cppad fmt rapidjson catch2
+    spack install
+    spack env deactivate
+fi
+
+echo "Building Coek"
+echo ""
 \rm -Rf build
 mkdir build
 cd build
